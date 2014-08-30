bd7fe7abaa7b303f28cdac4968ab3433fab3db9f
Lincoln Stuart <lincoln.stuartlg@live.com>
2014-08-30 14:00:20 -0300

    To GitHub

---
+++ remotecontroller/remotecontroller.ino
@@ -0,0 +1,32 @@
+#include <IRremote.h>
+
+int pinRemoteController = 13;
+int pinLed = 4;
+IRrecv irrecv(pinRemoteController);  
+decode_results dResults; 
+
+void setup(){
+  pinMode(pinRemoteController, OUTPUT);
+  pinMode(pinLed, OUTPUT);
+  irrecv.enableIRIn();
+  Serial.begin(9600);
+}
+
+void loop(){
+  if (irrecv.decode(&dResults)){
+    Serial.println(dResults.value, HEX);
+    float button = (dResults.value);
+    //20DF10EF is the code of the power button on the remote control(LG TV)
+    if(button == 0x20DF10EF){
+      if (digitalRead(pinLed) == HIGH){
+        Serial.println("TURN OFF");
+        digitalWrite(pinLed, LOW);
+      } else {
+        Serial.println("TURN ON");
+        digitalWrite(pinLed, HIGH);
+      }
+    }
+    irrecv.resume();
+  }
+}
+
